50fa02138dcc3a56a0a0591341c5f919cb437a3a
Adrian <adriankosyra@gmail.com>
2021-01-04 17:13:03 +0100

    Update adding_item_to_cart.js

--- tests/adding_item_to_cart.js
+++ tests/adding_item_to_cart.js
@@ -10,14 +10,22 @@ const inputPriceField = Selector('#price_from');
 const randomItemElement = Selector('section article').nth(1);
 const addToCartButton = Selector('button#add-to-cart-button');
 const goToCartButton = Selector('a[data-analytics-click-label="goToCart"]');
-const itemTitle = Selector('div h1');
+const itemTitleItemPage = Selector('div h1');
 const mainPageUrl = 'https://allegro.pl';
 
-const getItemTitle = Selector('div h1').innerText;
-
-const verifyIfCorrectItem = (title)=>{
-    return Selector("//a[contains(text(), '" + title + "')]")
-}
+/*
+Preconditions:
+1. Dowolny użytkownik
+2. Bez wcześniej zapisanych cookie
+Steps:
+1. Wejdź na główną stronę allegro.pl i zaznacz "Przejdź dalej" w oknie
+2. Wyszukaj przedmiot
+3. Zaznacz kolejno opcje: Używane, Kup teraz, Kwota minimalna = 2000
+4. Wybierz dowolny przedmiot z listy dostępnych
+5. Dodaj przedmiot do koszyka
+6. Przejdź do koszyka
+7. Sprawdź czy wybrany przedmiot znajduje się w koszyku
+*/
 
 fixture('Adding items to cart on classic allegro')
     .meta('category', 'cart')
@@ -35,10 +43,10 @@ test.page(mainPageUrl)('The used item should be added to the cart', async t =>{
         .click(randomItemElement())
         .click(addToCartButton());
 
-    const title = await itemTitle().innerText;
+    const title = await itemTitleItemPage().innerText;
     console.log("The title of the item is: " + title)
     await t
         .click(goToCartButton())
         .expect(Selector('offer-title').child().child(1).withText(title).exists).ok('W koszyku nie znaleziono oczekiwanego przedmiotu')
-        .debug()
+        //.debug()
 })
