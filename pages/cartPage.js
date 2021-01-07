9b41a98691b54bddd104264fec40c85897274def
Adrian <adriankosyra@gmail.com>
2021-01-07 19:21:55 +0100

    Update cartPage.js

--- pages/cartPage.js
+++ pages/cartPage.js
@@ -1,6 +1,9 @@
 import {Selector, t} from "testcafe";
 
 export const elements = {
+    buttons : {
+        buyButton : Selector('')
+    },
     goToCartButton : Selector('a[data-analytics-click-label="goToCart"]'),
     itemTitle : Selector('offer-title > div > div > a')
 }
